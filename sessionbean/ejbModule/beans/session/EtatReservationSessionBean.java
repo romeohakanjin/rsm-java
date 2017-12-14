f3d16d298fd89155485a9823afe09085e2575e22
Sindy-L <sindy.lim@outlook.fr>
2017-12-14 20:54:37 +0100

    session bean

---
+++ sessionbean/ejbModule/beans/session/EtatReservationSessionBean.java
@@ -0,0 +1,67 @@
+package beans.session;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+import beans.entity.EtatReservation;
+
+/**
+ * @author Sindy
+ */
+@Stateful
+@LocalBean
+@TransactionManagement(TransactionManagementType.BEAN)
+public class EtatReservationSessionBean {
+
+	@PersistenceContext(unitName = "AnnonceService")
+	EntityManager entityManager;
+
+	@Resource
+	UserTransaction userTransaction;
+
+	/**
+	 * Créer un etat de réservation
+	 * @param libelle
+	 * @return
+	 */
+	public Boolean creerEtatReservation(String libelle) {
+		try {
+			userTransaction.begin();
+			EtatReservation etat = new EtatReservation();
+			etat.setLibelle(libelle);
+			entityManager.persist(etat);
+			userTransaction.commit();
+			return true;
+		} catch (SecurityException | IllegalStateException | RollbackException | HeuristicMixedException
+				| HeuristicRollbackException | SystemException | NotSupportedException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Récupère tous les états de réservation
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<EtatReservation> getAllEtatReservation(){
+		String queryString = "FROM EtatReservation";
+		Query query = entityManager.createQuery(queryString);
+		return (List<EtatReservation>) query.getResultList();
+	}
+
+}
---
+++ sessionbean/ejbModule/beans/session/TypeUtilisateurSessionBean.java
@@ -0,0 +1,66 @@
+package beans.session;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+import beans.entity.TypeUtilisateur;
+
+/**
+ * @author Sindy
+ */
+@Stateful
+@LocalBean
+@TransactionManagement(TransactionManagementType.BEAN)
+public class TypeUtilisateurSessionBean {
+
+	@PersistenceContext(unitName = "AnnonceService")
+	EntityManager entityManager;
+
+	@Resource
+	UserTransaction userTransaction;
+
+	/**
+	 * Créer un type d'utilisateur
+	 * @param libelle
+	 * @return
+	 */
+	public Boolean creerTypeUtilisateur(String libelle) {
+		try {
+			userTransaction.begin();
+			TypeUtilisateur typeUtilisateur = new TypeUtilisateur();
+			typeUtilisateur.setLibelle(libelle);
+			entityManager.persist(typeUtilisateur);
+			userTransaction.commit();
+			return true;
+		} catch (SecurityException | IllegalStateException | RollbackException | HeuristicMixedException
+				| HeuristicRollbackException | SystemException | NotSupportedException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Récupère tous les types d'utilisateurs
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<TypeUtilisateur> getAllTypeUtilisateur(){
+		String queryString = "FROM TypeUtilisateur";
+		Query query = entityManager.createQuery(queryString);
+		return (List<TypeUtilisateur>) query.getResultList();
+	}
+}
